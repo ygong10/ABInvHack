e9fd826dffb79ec9960998545a4e7ca01e2cb28f
Yicheng Gong <Yicheng Gong>
2015-09-26 17:33:32 -0500

    sigh

--- mercury4json.py
+++ mercury4json.py
@@ -5,18 +5,29 @@ import urllib2, json, sys, time, csv, re, requests
 def main_loop():
      while 1:
 
-        url = "https://server.alcoholanalytics.com/api/hackathon/?m=579&d=c&f=&l=&s=&t=j"
-        response = urllib2.urlopen(url)
-        data = json.load(response)
-        print data
+         url = "https://server.alcoholanalytics.com/api/hackathon/?m=579&d=c&f=&l=&s=&t=j"
+         response = urllib2.urlopen(url)
+         data = json.load(response)
 
-        with open('mercuryj.txt', 'wb') as outfile:
-            for row in data:
+
+
+        # csvfile = open('mercuryc.csv', 'r')
+        # jsonfile = open('mercuryj.json', 'w')
+        #
+        # #fieldnames = ("FirstName","LastName","IDNumber","Message")
+        # reader = csv.reader(csvfile)
+        # for row in reader:
+        #     json.dump(row, jsonfile)
+        #     jsonfile.write('\n')
+        #     print row
+
+         with open("mercuryj.json", "wb") as outfile:
+             for row in data:
                 json.dump(row, outfile)
-                outfile.write('\n')
-                #print row
-        outfile.close
-        time.sleep(0.1)
+                outfile.write("\n")
+                print row
+
+         time.sleep(0.1)
 
 if __name__ == '__main__':
      try:
